7c0caac8b54c78f9a140cdeac659d63428024874
LesleyBeijl <99062581@mydavinci.nl>
2021-05-31 14:13:38 +0200

    lab1 verbeterd

--- L1/script.js
+++ L1/script.js
@@ -3,15 +3,16 @@ let btn = document.createElement("button");
 document.body.appendChild(btn);
 container.appendChild(btn);
 btn.innerText = i;
-var color = ["red", "purple", "blue", "black", "white"];
+var color = ["red", "purple", "blue", "black", "white", "yellow"];
 btn.onclick = function() {click (this,color) 
 	} 
 function click(button,color) {
 var color2 = color.indexOf(button.style.backgroundColor)
  button.style.backgroundColor = color[color2+1]
-if( button.style.backgroundColor == "white"){
+if( button.style.backgroundColor == "yellow"){
 	button.remove()	};
 }
 
 
 }
+
