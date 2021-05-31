ab8309d3546d056a929179ed617be79f232dff2b
LesleyBeijl <99062581@mydavinci.nl>
2021-05-31 10:26:37 +0200

    buttons changes

--- L1/script.js
+++ L1/script.js
@@ -3,5 +3,9 @@ let btn = document.createElement("button");
 document.body.appendChild(btn);
 container.appendChild(btn);
 btn.innerText = i;
+btn.onclick = function(a) {changecolor(this)};
 }
+
+function changecolor(a) {
+	button.backgroundcolor = a.backgroundcolor; }
 
