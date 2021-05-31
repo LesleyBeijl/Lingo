26e82362cca7dc5e4275ca901f9a746db1d1de0a
LesleyBeijl <99062581@mydavinci.nl>
2021-05-31 13:04:01 +0200

    Lab1 klaar

--- L1/script.js
+++ L1/script.js
@@ -3,6 +3,15 @@ let btn = document.createElement("button");
 document.body.appendChild(btn);
 container.appendChild(btn);
 btn.innerText = i;
-btn.onclick = function(a) {btn.style.backgroundColor = 'red'; 'purple'; 'blue'; 'black';};
+var color = ["red", "purple", "blue", "black", "white"];
+btn.onclick = function() {click (this,color) 
+	} 
+function click(button,color) {
+var color2 = color.indexOf(button.style.backgroundColor)
+ button.style.backgroundColor = color[color2+1]
+if( button.style.backgroundColor == "white"){
+	button.remove()	};
+}
+
 
 }
