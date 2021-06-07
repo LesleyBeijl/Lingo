3e9c8e82025738c137d226371423e69e2b341873
LesleyBeijl <99062581@mydavinci.nl>
2021-06-07 10:17:55 +0200

    check button

--- L4/script.js
+++ L4/script.js
@@ -1,9 +1,1 @@
-let btn = document.createElement("button");
-document.body.appendChild(btn);
-container.appendChild(btn);
 
-for (var i = 1; i < 6; i++) {
-let btn = document.createElement("button");
-document.body.appendChild(btn);
-container.appendChild(btn);
-}
